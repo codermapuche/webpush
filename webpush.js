433d2d6cc971ef9a911df87d5be324239337713e
Nehuen Prados <nehuensd@gmail.com>
2022-12-24 16:39:45 -0300

    Update standars

--- webpush.js
+++ webpush.js
@@ -1,33 +1,33 @@
 const https  = require('https'),
-			crypto = require('crypto'),
-			url 	 = require('url');
+      crypto = require('crypto'),
+      url    = require('url');
 
 // ------------------------------------------------------------------------
 // https://datatracker.ietf.org/doc/html/rfc5869
 // ------------------------------------------------------------------------
 
 function HKDF_expand(prk, info, l) {
-	let output = Buffer.alloc(0),
-			T = Buffer.alloc(0),
-			counter = 0,
-			cbuf = Buffer.alloc(1);
+  let output = Buffer.alloc(0),
+      T = Buffer.alloc(0),
+      counter = 0,
+      cbuf = Buffer.alloc(1);
 
-	info = Buffer.from(info, 'ascii');
+  info = Buffer.from(info, 'ascii');
 
-	while (output.length < l) {
-		cbuf.writeUIntBE(++counter, 0, 1);
-		T = HMAC_hash(prk, Buffer.concat([T, info, cbuf]));
-		output = Buffer.concat([output, T]);
-	}
+  while (output.length < l) {
+    cbuf.writeUIntBE(++counter, 0, 1);
+    T = HMAC_hash(prk, Buffer.concat([T, info, cbuf]));
+    output = Buffer.concat([output, T]);
+  }
 
-	return output.slice(0, l);
+  return output.slice(0, l);
 }
 
 function HMAC_hash(key, input) {
-	return crypto
-		.createHmac('sha256', key)
-		.update(input)
-		.digest();
+  return crypto
+    .createHmac('sha256', key)
+    .update(input)
+    .digest();
 }
 
 // ------------------------------------------------------------------------
@@ -35,18 +35,19 @@ function HMAC_hash(key, input) {
 // ------------------------------------------------------------------------
 
 function HKDF_extract(key, input) {
-	return HMAC_hash(key, input);
+  return HMAC_hash(key, input);
 }
 
 // ------------------------------------------------------------------------
 // https://datatracker.ietf.org/doc/html/rfc7515#appendix-C
 // ------------------------------------------------------------------------
 
-function base64urlencode(buff) {
-	return buff.toString('base64')
-    .split("=")[0]
-    .replace(/\+/g, "-")
-    .replace(/\//g, "_");
+function base64urlEncode(str) {
+  return base64urlEscape(Buffer.from(str).toString('base64'));
+}
+
+function base64urlEscape(str) {
+  return str.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
 }
 
 // ------------------------------------------------------------------------
@@ -54,76 +55,89 @@ function base64urlencode(buff) {
 // ------------------------------------------------------------------------
 
 function VAPID_generateKeys() {
-	const curve = crypto.createECDH('prime256v1');
-	curve.generateKeys();
+  const curve = crypto.createECDH('prime256v1');
+  curve.generateKeys();
 
-	let pubKey = curve.getPublicKey(),
-			prvKey = curve.getPrivateKey();
+  let privateKey = curve.getPrivateKey(),
+      publicKey  = curve.getPublicKey();
 
-	return {
-		cert : pubKey.toString('base64'),
-		key	 : '-----BEGIN EC PRIVATE KEY-----\n' +
-					 Buffer.from('30770201010420' +
-						prvKey.toString('hex') +
-						'A00A06082A8648CE3D030107A144034200' +
-						pubKey.toString('hex'), 'hex')
-						.toString('base64') + '\n' +
-					 '-----END EC PRIVATE KEY-----'
-	};
+  return {
+    cert  : base64urlEncode(publicKey),
+    key   : JSON.stringify({
+              key: {
+                kty : 'EC',
+                crv : 'P-256',
+                x   : base64urlEncode(publicKey.slice(1, 33)),
+                y   : base64urlEncode(publicKey.slice(33, 65)),
+                d   : base64urlEncode(privateKey)
+              },
+              format: 'jwk'
+            })
+  };
 }
+
+// ------------------------------------------------------------------------
+// https://datatracker.ietf.org/doc/html/rfc7518#section-3.4
+// ------------------------------------------------------------------------
 
 function VAPID_jwt(key, audience, subject, expire) {
-	var curr = new Date(),
-			offs = curr.getTimezoneOffset();
+  const curr = new Date(),
+        offs = curr.getTimezoneOffset(),
+        header = {
+          typ: 'JWT',
+          alg: 'ES256'
+        },
+        payload = {
+          aud: audience.protocol + '//' + audience.host,
+          exp: Math.floor((Date.now() - offs) / 1000) + expire,
+          sub: subject
+        };
 
-	const header = {
-					typ: 'JWT',
-					alg: 'ES256'
-				},
-				payload = {
-					aud: audience.protocol + '//' + audience.host,
-					exp: (Date.now() - offs + expire * 1000) / 1000,
-					sub: subject
-				};
-
-	return JWT_create(key, header, payload);
+  return JWT_create(key, header, payload);
 }
 
+// ------------------------------------------------------------------------
+// https://datatracker.ietf.org/doc/html/rfc8292#section-2.4
+// ------------------------------------------------------------------------
+
 function VAPID_request(endpoint, cipher, jwt, cert) {
+  
+  const options = {
+    method: 'POST',
+    host: endpoint.host,
+    path: endpoint.path,
+    headers: {
+      'TTL'             : 1000 * 60,
+      'Content-Encoding': 'aes128gcm',
+      'Content-Type'    : 'application/octet-stream',
+      'Content-Length'  : cipher.length,
+      'Authorization'   : 'vapid t=' + jwt + ', k=' + cert
+    }
+  };
 
-	const options = {
-		method: 'POST',
-		host: endpoint.host,
-		path: endpoint.path,
-		headers: {
-			'TTL'							: 1000 * 60,
-			'Content-Encoding': 'aes128gcm',
-			'Content-Type'		: 'application/octet-stream',
-			'Content-Length'	: cipher.length,
-			'Authorization'		: 'vapid t=' + jwt + ', k=' + cert
-		}
-	}
+  // ------------------------------------------------------------------------
 
-	// ------------------------------------------------------------------------
-
-	return new Promise((res, rej) => {
-		const req = https.request(options, (resp) => {
-      if ( resp.statusCode < 200 || resp.statusCode >= 300 ) {
-        return rej(resp.statusCode);
-      }
-
+  return new Promise((res, rej) => {
+    const req = https.request(options, (resp) => {
       const chunks = [];
       resp.on("data", (chunk) => (chunks.push(chunk)));
       resp.on("end", () => {
-				const body = Buffer.concat(chunks).toString();
-				res(body);
-			});
+        const body = Buffer.concat(chunks).toString();
+
+        if ( resp.statusCode < 200 || resp.statusCode >= 300 ) {
+          return rej({ body,
+            statusCode: resp.statusCode
+          });
+        }
+
+        res(body);
+      });
     });
 
     req.on("error", rej);
-		req.write(cipher);
+    req.write(cipher);
     req.end();
-	});
+  });
 }
 
 // ------------------------------------------------------------------------
@@ -131,32 +145,27 @@ function VAPID_request(endpoint, cipher, jwt, cert) {
 // ------------------------------------------------------------------------
 
 function JWT_create(key, header, payload) {
-	header  = JSON.stringify(header);
-	payload = JSON.stringify(payload);
+  header  = JSON.stringify(header);
+  payload = JSON.stringify(payload);
 
-	header  = Buffer.from(header);
-	payload = Buffer.from(payload);
+  header  = Buffer.from(header);
+  payload = Buffer.from(payload);
 
-	header  = base64urlencode(header);
-	payload = base64urlencode(payload);
+  header  = base64urlEncode(header);
+  payload = base64urlEncode(payload);
 
-	let sign = JWS_sign(header, payload, key);
-	sign = base64urlencode(sign);
+  let sign = JWS_sign(header, payload, key);
+  sign = base64urlEncode(sign);
 
-	return header + '.' + payload + '.' + sign;
+  return header + '.' + payload + '.' + sign;
 }
 
 // ------------------------------------------------------------------------
 // https://datatracker.ietf.org/doc/html/rfc7515
 // ------------------------------------------------------------------------
 
-function JWS_sign(header, payload, key) {
-	let token  = header + '.' + payload,
-			signer = crypto.createSign('RSA-SHA256');
-
-	signer.update(token);
-
-	return signer.sign(key, 'base64');
+function JWS_sign(header, payload, privateKey) {
+  return crypto.sign("SHA256", header + '.' + payload, privateKey);
 }
 
 // ------------------------------------------------------------------------
@@ -164,7 +173,7 @@ function JWS_sign(header, payload, key) {
 // ------------------------------------------------------------------------
 
 function RANDOM_salt(length) {
-	return crypto.randomBytes(length);
+  return crypto.randomBytes(length);
 }
 
 // ------------------------------------------------------------------------
@@ -173,7 +182,7 @@ function RANDOM_salt(length) {
 // ------------------------------------------------------------------------
 
 function ECDH(c_private, c_public) {
-	return c_private.computeSecret(c_public);
+  return c_private.computeSecret(c_public);
 }
 
 // ------------------------------------------------------------------------
@@ -181,12 +190,12 @@ function ECDH(c_private, c_public) {
 // ------------------------------------------------------------------------
 
 function generateNonce(base, counter) {
-	var nonce = Buffer.from(base);
-	var m = nonce.readUIntBE(nonce.length - 6, 6);
-	var x = ((m ^ counter) & 0xffffff) +
-			((((m / 0x1000000) ^ (counter / 0x1000000)) & 0xffffff) * 0x1000000);
-	nonce.writeUIntBE(x, nonce.length - 6, 6);
-	return nonce;
+  var nonce = Buffer.from(base);
+  var m = nonce.readUIntBE(nonce.length - 6, 6);
+  var x = ((m ^ counter) & 0xffffff) +
+      ((((m / 0x1000000) ^ (counter / 0x1000000)) & 0xffffff) * 0x1000000);
+  nonce.writeUIntBE(x, nonce.length - 6, 6);
+  return nonce;
 }
 
 // ------------------------------------------------------------------------
@@ -194,84 +203,91 @@ function generateNonce(base, counter) {
 // ------------------------------------------------------------------------
 
 const RECORD_SIZE = 4096,
-			AES_GCM 		= 'aes-128-gcm',
-			OVERHEAD 		= 16 + 1;
+      AES_GCM     = 'aes-128-gcm',
+      OVERHEAD    = 16 + 1;
 
 // ------------------------------------------------------------------------
 
 function push(pub, sub, data) {
 
-	data = Buffer.from(JSON.stringify(data));
+  data = Buffer.from(JSON.stringify(data));
 
-	if (RECORD_SIZE - OVERHEAD <= data.length) {
-		throw "Max data size of push is: " + (RECORD_SIZE - OVERHEAD);
-	}
+  if (RECORD_SIZE - OVERHEAD <= data.length) {
+    throw "Max data size of push is: " + (RECORD_SIZE - OVERHEAD);
+  }
 
-	// ------------------------------------------------------------------------
-	// https://datatracker.ietf.org/doc/html/rfc8291#section-2
-	// ------------------------------------------------------------------------
+  // ------------------------------------------------------------------------
+  // https://datatracker.ietf.org/doc/html/rfc8291#section-2
+  // ------------------------------------------------------------------------
 
-	let curve = crypto.createECDH('prime256v1'),
-			salt = RANDOM_salt(16),
-			keyid;
+  let curve = crypto.createECDH('prime256v1'),
+      salt = RANDOM_salt(16),
+      keyid;
 
-	curve.generateKeys();
-	keyid = curve.getPublicKey();
+  curve.generateKeys();
+  keyid = curve.getPublicKey();
 
-	// ------------------------------------------------------------------------
-	// https://datatracker.ietf.org/doc/html/rfc8291#section-3.4
-	// ------------------------------------------------------------------------
+  // ------------------------------------------------------------------------
+  // https://datatracker.ietf.org/doc/html/rfc8291#section-3.4
+  // ------------------------------------------------------------------------
 
-	let ua_public   = Buffer.from(sub.p256dh, 'base64'),
-			ecdh_secret = ECDH(curve, ua_public),
-			auth_secret = Buffer.from(sub.auth, 'base64'),
-			PRK_key  		= HKDF_extract(auth_secret, ecdh_secret),
-			key_str	    = Buffer.from('WebPush: info\0'),
-			key_info 		= Buffer.concat([ key_str, ua_public, keyid ]),
-			IKM 		 		= HKDF_expand(PRK_key, key_info, 32),
-			PRK         = HKDF_extract(salt, IKM),
-			cek_info 	  = Buffer.from('Content-Encoding: aes128gcm\0'),
-			CEK 		 		= HKDF_expand(PRK, cek_info, 16),
-			nonce_info 	= Buffer.from('Content-Encoding: nonce\0'),
-      NONCE 			= HKDF_expand(PRK, nonce_info, 12);
+  let ua_public   = Buffer.from(sub.p256dh, 'base64'),
+      ecdh_secret = ECDH(curve, ua_public),
+      auth_secret = Buffer.from(sub.auth, 'base64'),
+      PRK_key     = HKDF_extract(auth_secret, ecdh_secret),
+      key_str     = Buffer.from('WebPush: info\0'),
+      key_info    = Buffer.concat([ key_str, ua_public, keyid ]),
+      IKM         = HKDF_expand(PRK_key, key_info, 32),
+      PRK         = HKDF_extract(salt, IKM),
+      cek_info    = Buffer.from('Content-Encoding: aes128gcm\0'),
+      CEK         = HKDF_expand(PRK, cek_info, 16),
+      nonce_info  = Buffer.from('Content-Encoding: nonce\0'),
+      NONCE       = HKDF_expand(PRK, nonce_info, 12);
 
-	// ------------------------------------------------------------------------
-	// https://datatracker.ietf.org/doc/html/rfc8188#section-2.1
-	// ------------------------------------------------------------------------
+  // ------------------------------------------------------------------------
+  // https://datatracker.ietf.org/doc/html/rfc8188#section-2.1
+  // ------------------------------------------------------------------------
 
-	let ecch = Buffer.alloc(5);
-	ecch.writeUIntBE(RECORD_SIZE, 0, 4);
-	ecch.writeUIntBE(keyid.length, 4, 1);
-	ecch = Buffer.concat([ salt, ecch, keyid ]);
+  let ecch = Buffer.alloc(5);
+  ecch.writeUIntBE(RECORD_SIZE, 0, 4);
+  ecch.writeUIntBE(keyid.length, 4, 1);
+  ecch = Buffer.concat([ salt, ecch, keyid ]);
 
-	// ------------------------------------------------------------------------
-	// https://datatracker.ietf.org/doc/html/rfc8291#appendix-A
-	// ------------------------------------------------------------------------
+  // ------------------------------------------------------------------------
+  // https://datatracker.ietf.org/doc/html/rfc8291#appendix-A
+  // ------------------------------------------------------------------------
 
-	let nonce  = generateNonce(NONCE, 0),
-			cipher = crypto.createCipheriv(AES_GCM, CEK, nonce);
+  let nonce  = generateNonce(NONCE, 0),
+      cipher = crypto.createCipheriv(AES_GCM, CEK, nonce);
 
-	data = Buffer.concat([ data, Buffer.from([ 2 ]) ]);
-	data = cipher.update(data);
-	cipher.final();
+  data = Buffer.concat([ data, Buffer.from([ 2 ]) ]);
+  data = cipher.update(data);
+  cipher.final();
 
-	// ------------------------------------------------------------------------
+  // ------------------------------------------------------------------------
+  // https://datatracker.ietf.org/doc/html/rfc5349.html#section-7
+  // ------------------------------------------------------------------------
+  
+  let privateKey = JSON.parse(JSON.stringify(pub.key)); 
+  privateKey.dsaEncoding = 'ieee-p1363';
+  
+  // ------------------------------------------------------------------------
 
-	const endpoint = url.parse(sub.endpoint),
-				jwt = VAPID_jwt(pub.key, endpoint, pub.subject, 12 * 60 * 60);
+  const endpoint = url.parse(sub.endpoint),
+        jwt = VAPID_jwt(privateKey, endpoint, pub.subject, 12 * 60 * 60);
 
-	cipher = Buffer.concat([ ecch, data, cipher.getAuthTag() ]);
+  cipher = Buffer.concat([ ecch, data, cipher.getAuthTag() ]);
 
-	// ------------------------------------------------------------------------
+  // ------------------------------------------------------------------------
 
-	return VAPID_request(endpoint, cipher, jwt, pub.cert);
+  return VAPID_request(endpoint, cipher, jwt, pub.cert);
 }
 
 // --------------------------------------------------------------------------
 
 module.exports = {
-	VAPID_generateKeys,
-	push
+  VAPID_generateKeys,
+  push
 }
 
 // --------------------------------------------------------------------------
